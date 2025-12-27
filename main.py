1af81654f5da39bdf16218c4f14e66ee62342dc2
Dominikanayyuhu <veriksgames@gmail.com>
2025-12-27 18:10:14 +0400

    Update main.py

--- main.py
+++ main.py
@@ -20,24 +20,15 @@ ADMIN_ID = 2039589760
 bot = telebot.TeleBot(API_TOKEN)
 
 user_orders = {}
-# Список тех, кто уже сделал заказ
-completed_orders = set()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    cid = message.chat.id
-    if cid in completed_orders:
-        bot.send_message(cid, "Вы уже сделали заказ, пожалуйста подождите какое-то время")
-        return
     shop_link = "https://dominikanayyuhu.github.io/Ilfan-Pose-Shop/" 
-    bot.send_message(cid, f"👋 Привет! Магазин тут: {shop_link}\n🎬 Пришли видео оплаты для заказа.")
+    bot.send_message(message.chat.id, f"👋 Привет! Магазин тут: {shop_link}\n🎬 Пришли видео оплаты для заказа.")
 
 @bot.message_handler(content_types=['video', 'video_note'])
 def handle_payment_video(message):
     cid = message.chat.id
-    if cid in completed_orders:
-        bot.send_message(cid, "Вы уже сделали заказ, пожалуйста подождите какое-то время")
-        return
     user_orders[cid] = {
         'video_id': message.video.file_id if message.video else message.video_note.file_id,
         'state': 'WAITING_PHOTO'
@@ -47,9 +38,6 @@ def handle_payment_video(message):
 @bot.message_handler(content_types=['photo'])
 def handle_background(message):
     cid = message.chat.id
-    if cid in completed_orders:
-        bot.send_message(cid, "Вы уже сделали заказ, пожалуйста подождите какое-то время")
-        return
     if cid in user_orders and user_orders[cid].get('state') == 'WAITING_PHOTO':
         user_orders[cid]['photo_id'] = message.photo[-1].file_id
         user_orders[cid]['state'] = 'WAITING_NICKNAME'
@@ -60,10 +48,6 @@ def handle_background(message):
 @bot.message_handler(func=lambda m: True, content_types=['text'])
 def handle_all_text(message):
     cid = message.chat.id
-    if cid in completed_orders:
-        bot.send_message(cid, "Вы уже сделали заказ, пожалуйста подождите какое-то время")
-        return
-    
     if cid not in user_orders:
         bot.send_message(cid, "Напиши /start, чтобы начать заказ.")
         return
@@ -94,35 +78,38 @@ def handle_all_text(message):
         if choice in ["Робуксы", "мм2 годли", "Телеграм-звёзды"]:
             user_orders[cid]['payment_method'] = choice
             if choice == "Робуксы":
-                msg = "💵 Вы выбрали робуксы, оплатите по ссылке: https://www.roblox.com/games/18925562723/Позинги"
+                msg = "💵 Вы выбрали робуксы, пожалуйста оплатите заказ по ссылке: https://www.roblox.com/games/18925562723/"
             elif choice == "мм2 годли":
-                msg = "🔪 Вы выбрали годли, свяжитесь с владельцем (@HokhikyanHokhikyans) для оплаты."
+                msg = "🔪 Вы выбрали годли, пожалуйста свяжитесь с владельцем (@HokhikyanHokhikyans), чтобы оплатить заказ."
             else:
-                msg = "🌟 Вы выбрали звезды, оплатите по юзернейму @HokhikyanHokhikyans"
+                msg = "🌟 Вы выбрали телеграм звёзды, пожалуйста оплатите заказ по этому юзернейму (@HokhikyanHokhikyans)."
 
             markup = types.InlineKeyboardMarkup()
             markup.add(types.InlineKeyboardButton("✅ Я ОПЛАТИЛ(А)", callback_data="confirm_pay"))
             bot.send_message(cid, msg, reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda call: call.data == "confirm_pay")
-def final_step(call):
-    cid = call.message.chat.id
-    if cid in user_orders and 'payment_method' in user_orders[cid]:
-        order = user_orders[cid]
-        summary = (f"🔔 НОВЫЙ ЗАКАЗ!\n"
-                   f"👤 Заказчик: {order['nickname']}\n"
-                   f"👥 Кол-во персонажей: {order['chars_count']}\n"
-                   f"💰 Оплата: {order['payment_method']}")
-        
-        bot.send_message(ADMIN_ID, summary)
-        bot.send_video(ADMIN_ID, order['video_id'], caption="Видео оплаты")
-        bot.send_photo(ADMIN_ID, order['photo_id'], caption="Фон")
-        
-        # Добавляем пользователя в список выполнивших заказ
-        completed_orders.add(cid)
-        
-        bot.send_message(cid, "🚀 Все данные переданы! Ожидайте заказ. Владелец скоро свяжется с вами.", reply_markup=types.ReplyKeyboardRemove())
-        del user_orders[cid]
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    if call.data == "confirm_pay":
+        cid = call.message.chat.id
+        if cid in user_orders and 'payment_method' in user_orders[cid]:
+            order = user_orders[cid]
+            summary = (f"🔔 НОВЫЙ ЗАКАЗ!\n"
+                       f"👤 Заказчик: {order['nickname']}\n"
+                       f"👥 Кол-во персонажей: {order['chars_count']}\n"
+                       f"💰 Оплата: {order['payment_method']}")
+            
+            # Отправляем админу
+            bot.send_message(ADMIN_ID, summary)
+            bot.send_video(ADMIN_ID, order['video_id'], caption="Видео оплаты")
+            bot.send_photo(ADMIN_ID, order['photo_id'], caption="Фон")
+            
+            # Ответ клиенту
+            bot.send_message(cid, "🚀 Все данные переданы! Ожидайте заказ. Владелец скоро свяжется с вами.", reply_markup=types.ReplyKeyboardRemove())
+            # Очищаем данные заказа после отправки
+            del user_orders[cid]
+        else:
+            bot.send_message(cid, "Пожалуйста, сначала заполни все данные заказа!")
 
 if __name__ == "__main__":
     keep_alive()
