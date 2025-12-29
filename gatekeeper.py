99870064291517e15b4b215788f9974fdda58534
Dominikanayyuhu <veriksgames@gmail.com>
2025-12-29 16:37:44 +0400

    Update gatekeeper.py

--- gatekeeper.py
+++ gatekeeper.py
@@ -7,7 +7,7 @@ def check_access():
     """
     now = datetime.now()
     # Период отпуска: с 30 декабря 2025 по 6 января 2026 включительно
-    vacation_start = datetime(2025, 12, 30, 0, 0)
+    vacation_start = datetime(2025, 12, 28, 0, 0)
     vacation_end = datetime(2026, 1, 7, 0, 0) # Откроется 7 января
 
     if vacation_start <= now < vacation_end:
